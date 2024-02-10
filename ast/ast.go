9e3774d1971d14962d21057de3b0bba63c7da681
tmatsuba <tma@gside.org>
2024-02-10 22:14:31 +0900

    parse string.

--- ast/ast.go
+++ ast/ast.go
@@ -124,6 +124,15 @@ func (il *IntegerLiteral) expressionNode()      {}
 func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
 func (il *IntegerLiteral) String() string       { return il.Token.Literal }
 
+type StringLiteral struct {
+	Token token.Token
+	Value string
+}
+
+func (sl *StringLiteral) expressionNode() {}
+func (sl *StringLiteral) TokenLiteral() string { return sl.TokenLiteral() }
+func (sl *StringLiteral) String() string { return sl.Token.Literal }
+
 type PrefixExpression struct {
 	Token    token.Token
 	Operator string
@@ -264,3 +273,4 @@ func (ce *CallExpression) String() string {
 
 	return out.String()
 }
+
